bb355c6c2a43eede3fefe762d54bf6ece6cfe9de
Kevin Bi <keveloper@gmail.com>
2023-12-03 09:09:57 -0500

    Solve day 1

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -4,6 +4,39 @@ fn main() {
     println!("{}", solve(get_input_lines()));
 }
 
-fn solve(lines: impl Iterator<Item = String>) -> u64 {
-    unimplemented!()
+fn solve(lines: impl Iterator<Item = impl AsRef<str>>) -> u32 {
+    lines.map(|line| get_number(line.as_ref())).sum()
+}
+
+fn get_number(line: &str) -> u32 {
+    if line.is_empty() {
+        return 0;
+    }
+    let first_digit = line
+        .chars()
+        .find_map(|c| c.to_digit(10))
+        .expect("Did not find first digit");
+    let last_digit = line
+        .chars()
+        .rev()
+        .find_map(|c| c.to_digit(10))
+        .expect("Did not find last digit");
+    10 * first_digit + last_digit
+}
+
+#[cfg(test)]
+mod test {
+    use crate::solve;
+
+    #[test]
+    fn test_example() {
+        let input = r#"
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"#;
+
+        assert_eq!(solve(input.lines()), 142);
+    }
 }
