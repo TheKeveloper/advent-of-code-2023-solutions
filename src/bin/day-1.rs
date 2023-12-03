e9691a446532ffa5b13db71ed87f41014009dd9d
Kevin Bi <keveloper@gmail.com>
2023-12-03 08:54:20 -0500

    Set up input parsing

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -1,3 +1,9 @@
+use advent_of_code_2023_solutions::get_input_lines;
+
 fn main() {
-    println!("Hello world");
+    println!("{}", solve(get_input_lines()));
+}
+
+fn solve(lines: impl Iterator<Item = String>) -> u64 {
+    unimplemented!()
 }
