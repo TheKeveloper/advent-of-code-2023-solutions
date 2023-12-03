f98e7b9f99efe79006e47a9e2a55f7dd25e2b5a9
Kevin Bi <keveloper@gmail.com>
2023-12-03 09:11:09 -0500

    Solve day 1 (#2)
    * Set up input parsing
    
    * Solve day 1

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+pub fn get_input_path() -> PathBuf {
+    PathBuf::from(
+        env::args()
+            .nth(1)
+            .expect("Required one command line argument"),
+    )
+}
+
+pub fn get_input_lines() -> impl Iterator<Item = String> {
+    let file = File::open(get_input_path()).expect("Could not open input path");
+    let reader = BufReader::new(file);
+    reader
+        .lines()
+        .map(|result| result.expect("Could not read line from input file"))
+}
