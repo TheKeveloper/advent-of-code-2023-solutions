496e5c791a5d1c0f8c0ac427d60bc122587360d5
Kevin Bi <keveloper@gmail.com>
2023-12-03 23:06:57 -0500

    Refactoring things to be a bit more clean (#5)

---
+++ src/bin/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+use advent_of_code_2023_solutions::common::Solution;
+use advent_of_code_2023_solutions::day1::Day1;
+use advent_of_code_2023_solutions::day2::Day2;
+use advent_of_code_2023_solutions::day3::Day3;
+
+fn main() {
+    let cli = Cli::from_args();
+
+    let file = File::open(cli.input).expect("Could not read input");
+    let reader = BufReader::new(file);
+    let lines = reader
+        .lines()
+        .map(|result| result.expect("Could not read line from input file"));
+    let result = match (cli.day, cli.part) {
+        (1, 1) => Day1::solve(lines),
+        (2, 1) => Day2::solve(lines),
+        (3, 1) => Day3::solve(lines),
+        _ => unimplemented!(),
+    };
+    println!("{}", result)
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    day: u8,
+    part: u8,
+    input: PathBuf,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,4 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
-
-pub fn get_input_path() -> PathBuf {
-    PathBuf::from(
-        env::args()
-            .nth(1)
-            .expect("Required one command line argument"),
-    )
-}
-
-pub fn get_input_lines() -> impl Iterator<Item = String> {
-    let file = File::open(get_input_path()).expect("Could not open input path");
-    let reader = BufReader::new(file);
-    reader
-        .lines()
-        .map(|result| result.expect("Could not read line from input file"))
-}
-
-pub trait Solution {
-    fn default_print_solution() {
-        println!("{}", Self::solve(get_input_lines()));
-    }
-
-    fn solve(lines: impl Iterator<Item = impl AsRef<str>>) -> u32;
-}
+pub mod common;
+pub mod day1;
+pub mod day2;
+pub mod day3;
