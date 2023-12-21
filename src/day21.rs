a4217355bb9dde7fa8ee88a02425995662f95fa0
Kevin Bi <keveloper@gmail.com>
2023-12-21 10:43:52 -0600

    Solve part 2

--- src/day21.rs
+++ src/day21.rs
@@ -15,10 +15,27 @@ impl Solution for Day21 {
 pub enum Day21P2 {}
 impl Solution for Day21P2 {
     fn solve(lines: impl Iterator<Item = impl AsRef<str>>) -> String {
-        panic!(
-            "lines: {:?}",
-            lines.map(|s| s.as_ref().to_string()).collect::<Vec<_>>()
-        )
+        let grid = InfiniteGrid::from_lines(lines);
+
+        let grid_len = grid.tiles.num_rows();
+
+        let mut reachable = HashSet::new();
+        reachable.insert(grid.get_start().into());
+
+        // the total steps is 26501365 = grid_len * 202300 + 65
+        // we want to get past the first 65 steps and then use a quadratic equation at each grid
+        // boundary. The quadratic pattern is from someone on reddit.
+        for i in 0..=327 {
+            if (i - 65) % grid_len == 0 {
+                println!("{}, {}", i, reachable.len());
+            }
+            reachable = grid.one_step_reachable(reachable.iter());
+        }
+
+        // take the printed values, use an online quadratic regression, and with values 1, 2, 3
+        // then put in 202300 as the input.
+
+        "".to_string()
     }
 }
 
@@ -67,6 +84,104 @@ impl Grid {
     }
 }
 
+struct InfiniteGrid {
+    tiles: Vec2d<Tile>,
+}
+
+impl InfiniteGrid {
+    pub fn from_lines(lines: impl Iterator<Item = impl AsRef<str>>) -> Self {
+        InfiniteGrid {
+            tiles: Vec2d::from_lines(lines).map(|&c| c.into()),
+        }
+    }
+
+    pub fn get_start(&self) -> Cell<Tile> {
+        self.tiles
+            .cells()
+            .find(|cell| cell.value().is_start())
+            .unwrap()
+    }
+
+    pub fn one_step_reachable<'a>(
+        &self,
+        points: impl Iterator<Item = &'a Point>,
+    ) -> HashSet<Point> {
+        let mut set = HashSet::new();
+        for point in points {
+            point
+                .neighbors()
+                .iter()
+                .filter(|p| !self.get_value_on_grid(p).is_rock())
+                .for_each(|&p| {
+                    set.insert(p);
+                })
+        }
+        set
+    }
+
+    pub fn start_reachable_n_steps(&self, steps: usize) -> usize {
+        let mut reachable = HashSet::new();
+        reachable.insert(self.get_start().into());
+
+        for _ in 0..steps {
+            reachable = self.one_step_reachable(reachable.iter());
+        }
+
+        reachable.len()
+    }
+
+    fn get_value_on_grid(&self, point: &Point) -> Tile {
+        let row = point.row.rem_euclid(self.tiles.num_rows() as i64);
+        let col = point.col.rem_euclid(self.tiles.first_num_cols() as i64);
+        self.tiles.get(row as usize, col as usize).unwrap().clone()
+    }
+}
+
+#[derive(Eq, PartialEq, Hash, Clone, Copy)]
+struct Point {
+    row: i64,
+    col: i64,
+}
+
+impl Point {
+    pub fn neighbors(&self) -> [Point; 4] {
+        [self.above(), self.right(), self.below(), self.left()]
+    }
+
+    fn diff(&self, row_diff: i64, col_diff: i64) -> Point {
+        Point {
+            row: self.row + row_diff,
+            col: self.col + col_diff,
+        }
+    }
+
+    fn above(&self) -> Point {
+        self.diff(-1, 0)
+    }
+
+    fn below(&self) -> Point {
+        self.diff(1, 0)
+    }
+
+    fn left(&self) -> Point {
+        self.diff(0, -1)
+    }
+
+    fn right(&self) -> Point {
+        self.diff(0, 1)
+    }
+}
+
+impl<'a, T> From<Cell<'a, T>> for Point {
+    fn from(value: Cell<'a, T>) -> Self {
+        Point {
+            row: value.row() as i64,
+            col: value.col() as i64,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
 enum Tile {
     Starting,
     Garden,
@@ -96,8 +211,7 @@ impl From<char> for Tile {
 
 #[cfg(test)]
 mod test {
-    use crate::common::Solution;
-    use crate::day21::{Day21P2, Grid};
+    use crate::day21::{Grid, InfiniteGrid};
 
     const EXAMPLE_INPUT: &str = r"...........
 .....###.#.
@@ -117,8 +231,10 @@ mod test {
     }
 
     #[test]
-    #[should_panic]
     fn test_example_p2() {
-        assert_eq!(Day21P2::solve(EXAMPLE_INPUT.lines()), "")
+        let grid = InfiniteGrid::from_lines(EXAMPLE_INPUT.lines());
+        assert_eq!(grid.start_reachable_n_steps(6), 16);
+        assert_eq!(grid.start_reachable_n_steps(10), 50);
+        assert_eq!(grid.start_reachable_n_steps(50), 1594);
     }
 }
